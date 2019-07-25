50e39f23875c1fbd6a4d141a57d431688dc9a940
Salvador Martinez <salvadormartinez022693@gmail.com>
2019-07-25 15:09:32 -0500

    Fix amount triple digit cents

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -7,7 +7,7 @@ const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
     <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
     <p>
-      {numeral(amount / 100).format('$0,0.00 ')}
+      {numeral(amount / 100).format('$0,0.00')}
       -
       {moment(createdAt).format(' ddd, MMMM Do, YYYY')}
     </p>
